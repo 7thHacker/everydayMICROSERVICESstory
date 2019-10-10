06abff44ec68ca2fb084883f0857346ff71dd743
7thHacker <everydayCSstory@gmail.com>
2019-10-10 15:19:00 +0530

    everydayMicroservicEstory v0.0.1
    -- updated "Hello Microservices" message to use name of the user to welcome them in microservices

--- src/main/java/com/everydayCODEstory/EverydayMicroservicEstoryApplication.java
+++ src/main/java/com/everydayCODEstory/EverydayMicroservicEstoryApplication.java
@@ -2,21 +2,22 @@ package com.everydayCODEstory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping(value="hello")
+@RequestMapping(value = "hello")
 public class EverydayMicroservicEstoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EverydayMicroservicEstoryApplication.class, args);
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String sayHello() {
-		return "Hello Microservices";
+	@RequestMapping(method = RequestMethod.GET, value = "/{userName}")
+	public String sayHello(@PathVariable("userName") String userName) {
+		return "Hello " + userName + " Welcome to Microservices";
 	}
 }
